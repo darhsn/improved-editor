f6ebe9c74a2244124eed893b77323c91d63d39a0
Dario <76775977+darioisthebest@users.noreply.github.com>
2021-09-01 08:33:04 +0200

    Added main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+ ___ __  __                              _ _____    _ _ _
+|_ _|  \/  |_ __  _ __ _____   _____  __| | ____|__| (_) |_ ___  _ __
+ | || |\/| | '_ \| '__/ _ \ \ / / _ \/ _` |  _| / _` | | __/ _ \| '__|
+ | || |  | | |_) | | | (_) \ V /  __/ (_| | |__| (_| | | || (_) | |
+|___|_|  |_| .__/|_|  \___/ \_/ \___|\__,_|_____\__,_|_|\__\___/|_|
+           |_|
+    D  e  f  a  u  l  t   C  o  n  f  i  g  u  r  a  t  i  o  n
+
+ Default IME configuration by darhsn
+"""
+import ime
+
+# Create new ImeInstance
+editor = ime.ImeInstance(default_buf="Untitled", name="Ime User")
+
+# Startup editor
+editor.start()
